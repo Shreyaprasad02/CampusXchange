8f0b05dda6ae789da477fe46e99a305c7ed8b0f0
Shreya <shreyaprasadaug@gmail.com>
2023-08-07 07:22:06 +0530

    Initial commit

--- campusxchange/src/HeaderOption.js
+++ campusxchange/src/HeaderOption.js
@@ -2,9 +2,12 @@ import React from 'react'
 import './HeaderOption.css'
 
 function HeaderOption({Icon, title}) {
-  return<div className='headerOption'>
+  return(
+    <div className='headerOption'>
       {Icon && <Icon className='headerOption__icon'/>}
-    </div>;
+      <h3 className="headerOption__title">{title}</h3>
+    </div>
+  );
 }
 
 export default HeaderOption;
