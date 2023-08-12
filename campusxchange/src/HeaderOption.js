11cc33db47c667496c63d0eb780e00efa4cc2109
Shreya <shreyaprasadaug@gmail.com>
2023-08-12 09:42:07 +0530

    Initial commit

--- campusxchange/src/HeaderOption.js
+++ campusxchange/src/HeaderOption.js
@@ -5,7 +5,7 @@ function HeaderOption({Icon, title}) {
   return(
     <div className='headerOption'>
       {Icon && <Icon className='headerOption__icon'/>}
-      <h3 className="headerOption__title">{title}</h3>
+      <h4 className="headerOption__title">{title}</h4>
     </div>
   );
 }
