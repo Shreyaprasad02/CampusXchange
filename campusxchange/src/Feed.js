99544303a7692041b0d4524e591a10b6d4435d56
Shreya <shreyaprasadaug@gmail.com>
2023-08-14 19:57:33 +0530

    Initial commit

--- campusxchange/src/Feed.js
+++ campusxchange/src/Feed.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React from 'react';
 import './Feed.css';
-import CreateIcon from '@material-ui/icons/Create'
+import CreateIcon from '@material-ui/icons/Create';
+import PhotoIcon from '@material-ui/icons/Photo';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import TodayIcon from '@material-ui/icons/Today';
+import AssignmentIcon from '@material-ui/icons/Assignment';
 
 function Feed() {
   return <div className='feed'>
@@ -8,13 +12,32 @@ function Feed() {
         <div className='feed__input'>
             <CreateIcon />
             <form>
-                <input type='text'/>
+                <input type='text' placeholder='Start a post'/>
                 <button type='submit'>Send</button>
             </form>
         </div>
 
-        <div className='feed_inputOptions'>
-            {/* <InputOption /> */}
+        <div className='feed__Options'>
+            <div className='option'>
+                <PhotoIcon style={{color:'#70b5f9'}}/> 
+                <span>Photo</span>
+            </div>
+
+            <div className='option'>
+                <YouTubeIcon style={{color:'#7fc15e'}} /> 
+                <span>Video</span>
+            </div>
+
+            <div className='option'>
+                <TodayIcon style={{color:'#e7a33e'}}/> 
+                <span>Event</span>
+            </div>
+
+            <div className='option'>
+                <AssignmentIcon style={{color:'#fc9295'}}/> 
+                <span>Write article</span>
+            </div>
+            
         </div>
     </div>
   </div>;
