af678da2dd44dec9582592f7f7fcd14a55db26a7
Shreya <shreyaprasadaug@gmail.com>
2023-09-17 16:23:43 +0530

    Initial commit

--- campusxchange/src/Header.js
+++ campusxchange/src/Header.js
@@ -3,10 +3,11 @@ import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import HeaderOption from './HeaderOption';
 import HomeIcon from "@material-ui/icons/Home";
-import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+// import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
 import ChatIcon from '@material-ui/icons/Chat';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import AssistantIcon from '@material-ui/icons/Assistant';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 function Header() {
@@ -19,10 +20,11 @@ function Header() {
 
       <div className = "header__center">
           <HeaderOption Icon={HomeIcon} title='Home' />
-          <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
-          <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
+          {/* <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/> */}
           <HeaderOption Icon={ChatIcon} title='Messaging'/>
+          <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
           <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+          <HeaderOption Icon={AssistantIcon} title='Assistant'/>
           <HeaderOption avatar={AccountCircleIcon} title='Me'/>
       </div>
 
