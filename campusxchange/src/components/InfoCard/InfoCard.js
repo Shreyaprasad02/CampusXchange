1f3f676d00b5cd61787f97de0b5fce4d4d58561a
Shreya <shreyaprasadaug@gmail.com>
2023-09-24 17:26:19 +0530

    Initial commit

---
+++ campusxchange/src/components/InfoCard/InfoCard.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import './InfoCard.css'
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
+
+const InfoCard = () => {
+  return (
+    <div className='InfoCard'>
+      <div className='infoHead'>
+        <h4>Your Info</h4>
+        <div>
+        <EditOutlinedIcon />
+        </div>
+      </div>
+
+      <div className="info">
+        <span>
+          <b>Pronouns </b>
+        </span>
+        <span>She/Her</span>
+      </div>
+      
+      <div className="info">
+        <span>
+          <b>Bio </b>
+        </span>
+        <span>Aspiring Software Developer</span>
+      </div>
+
+      <div className="info">
+        <span>
+          <b>Batch </b>
+        </span>
+        <span>2024</span>
+      </div>
+
+      <div className="info">
+        <span>
+          <b>Location </b>
+        </span>
+        <span>Pune</span>
+      </div>
+
+      <div className="info">
+        <span>
+          <b>Works at </b>
+        </span>
+        <span>Google</span>
+      </div>
+
+      <button className="button logout-button">Logout</button>
+    </div>
+  )
+}
+
+export default InfoCard
