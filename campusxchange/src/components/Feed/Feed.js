b8c900a51cb983cbcca27718f481da6c37e7755b
Shreya <shreyaprasadaug@gmail.com>
2023-09-24 17:26:48 +0530

    Initial commit

--- campusxchange/src/components/Feed/Feed.js
+++ campusxchange/src/components/Feed/Feed.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Feed.css';
 import PostShare from '../PostShare/PostShare';
+import Posts from '../Posts/Posts';
 
 function Feed() {
   return <div className='Feed'>
     <div>
         <PostShare />
+        <Posts />
     </div>
   </div>;
 }
